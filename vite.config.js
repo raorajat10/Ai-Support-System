86eca2cf33a7abd992eda787329fe7148bc56c9b
Rajatyadav <yadupatirajat@gmail.com>
2025-11-08 21:36:51 +0530

    build dist folder output dir in vite conf jd

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,7 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   ssr: false,
+   build: {
+    outDir: 'dist', // ensures Vite outputs to /dist
+  },
 });
